3a61dea28770d8ad1d83449650abbdf34328e0c6
doujiang24 <doujiang24@gmail.com>
2021-06-15 21:45:06 +0800

    tests: added a jit record test: reach the maxsnap limitation when recoding nyi.
    for this bugfix https://github.com/LuaJIT/LuaJIT/commit/3f9389edc6cdf3f78a6896d550c236860aed62b2

---
+++ test/misc/jit_record.lua
@@ -0,0 +1,31 @@
+require "jit.opt".start("hotloop=2", "hotexit=1", "maxsnap=9")
+
+local m = 1
+
+local function foo(n)
+    m = m + n
+    if n >= 2 then
+        m = m * n
+    end
+end
+
+foo(1); foo(1); foo(1); foo(1); foo(1); foo(1);
+
+local tb = {
+    foo = 1,
+    bar = 2,
+}
+
+local function bar()
+    for i = 2, 3 do
+        foo(i);
+    end
+
+    tb.foo = 10
+
+    print(tb.foo)
+end
+
+bar()
+
+print(m)
